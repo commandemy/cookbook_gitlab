d386e4c2da047ccff4e8b4c889be78953a66a28c
Jürgen Brüder <juergen.brueder@gmail.com>
2014-10-10 09:56:02 +0200

    inital commit

---
+++ recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: gitlab
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "apt"
+
+# Download Omnibus package and install dependencies
+remote_file "#{Chef::Config[:file_cache_path]}/gitlab_7.3.2-omnibus-1_amd64.deb" do
+	source "https://downloads-packages.s3.amazonaws.com/ubuntu-12.04/gitlab_7.3.2-omnibus-1_amd64.deb"
+end
+
+apt_package "openssh-server"
+include_recipe "postfix"
+
+dpkg_package "#{Chef::Config[:file_cache_path]}/gitlab_7.3.2-omnibus-1_amd64.deb" do
+	action :install
+end
+
+# Set hostname
+template "/etc/gitlab/gitlab.rb" do
+  source "gitlab.rb"
+  action :create
+end
+
+# Install and start Gitlab
+execute 'sudo gitlab-ctl reconfigure'
---
+++ templates/default/gitlab.rb
@@ -0,0 +1,2 @@
+# Change the external_url to the address your users will type in their browser
+external_url 'http://10.11.12.4'
