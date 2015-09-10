ba03b9d4f1de6e84b50c4f0e5e357bd305ffbeb6
Jürgen Brüder <juergen.brueder@gmail.com>
2015-09-10 13:45:10 +0200

    updated gitlab version to 7.14.2

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,23 +7,23 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "apt"
+include_recipe 'apt'
 
 # Download Omnibus package and install dependencies
-remote_file "#{Chef::Config[:file_cache_path]}/gitlab-ce_7.12.2~omnibus.1-1_amd64.deb" do
-	source "https://downloads-packages.s3.amazonaws.com/ubuntu-12.04/gitlab-ce_7.12.2~omnibus.1-1_amd64.deb"
+remote_file "#{Chef::Config[:file_cache_path]}/gitlab-ce_7.14.2-ce.1_amd64.deb" do
+  source 'https://packages.gitlab.com/gitlab/gitlab-ce/packages/ubuntu/precise/gitlab-ce_7.14.2-ce.1_amd64.deb'
 end
 
-apt_package "openssh-server"
-include_recipe "postfix"
+apt_package 'openssh-server'
+include_recipe 'postfix'
 
 dpkg_package "#{Chef::Config[:file_cache_path]}/gitlab-ce_7.12.2~omnibus.1-1_amd64.deb" do
-	action :install
+  action :install
 end
 
 # Set hostname
-template "/etc/gitlab/gitlab.rb" do
-  source "gitlab.rb"
+template '/etc/gitlab/gitlab.rb' do
+  source 'gitlab.rb'
   action :create
 end
 
